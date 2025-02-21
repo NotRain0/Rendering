6f76feb6c74ee6fa1ff93f3ad0cd0ca916376c72
NotRain0 <gr3goire.faure@gmail.com>
2025-02-21 18:54:04 +0100

    made square blue and top right

--- res/vertex.glsl
+++ res/vertex.glsl
@@ -4,5 +4,11 @@ layout(location = 0) in vec2 in_position;
 
 void main()
 {
-    gl_Position = vec4(in_position, 0., 1.);
+    vec2 position = in_position;
+    position.x += 0.4;
+    position.y += 0.4;
+    // ...
+    // Modifiez `position` comme vous voulez
+    // ...
+    gl_Position = vec4(position, 0., 1.); // Ici on utilise maintenant `position` et non plus `in_position`
 }
