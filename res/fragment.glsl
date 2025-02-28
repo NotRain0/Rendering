d98e18a5aeae2b31c1a9f270cb6eb51a195b10ae
NotRain0 <gr3goire.faure@gmail.com>
2025-02-28 12:46:36 +0100

    Shader multicolore

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -1,8 +1,13 @@
 //fragment.glsl
 
 #version 410
+
+in vec3 vertex_position; // Recevoir la position du vertex
+
 out vec4 out_color;
 
 void main() {
-    out_color = vec4(0.0, 1.0, 1.0, 1.0); // Couleur cyan
+    // Utilise la position du vertex comme couleur
+    // Normalise la position pour l'utiliser comme couleur (les positions peuvent être négatives)
+    out_color = vec4((vertex_position + 1.0) / 2.0, 1.0);
 }
--- res/vertex.glsl
+++ res/vertex.glsl
@@ -1,9 +1,13 @@
 //Vertex.glsl 
 
 #version 410
+
 uniform mat4 view_projection_matrix;
 layout(location = 0) in vec3 in_position;
 
+out vec3 vertex_position; // Variable transmise au fragment shader
+
 void main() {
     gl_Position = view_projection_matrix * vec4(in_position, 1.0);
+    vertex_position = in_position; // Assigner la position du vertex pour le passer au fragment shader
 }
