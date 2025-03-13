7f980e63bb9147e01f88c6fb8acbf867268a0091
NotRain0 <gr3goire.faure@gmail.com>
2025-03-13 16:59:49 +0100

    Lumiere ponctuelle qui tourne autour du bateau

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -4,7 +4,9 @@
 
 in vec2 frag_uv;
 in vec3 frag_normal;
+in vec3 frag_position;
 
+uniform vec3 light_position;
 uniform vec3 light_direction;
 uniform sampler2D my_texture;
 
@@ -12,10 +14,23 @@ out vec4 out_color;
 
 void main() {
     vec3 normal = normalize(frag_normal);
-    float diffuse = max(dot(normal, -normalize(light_direction)), 0.0);
+
+    float diffuse_dir = max(dot(normal, -normalize(light_direction)), 0.0);
+
+    vec3 light_vector = light_position - frag_position; // Vecteur lumière → pixel
+    float distance = length(light_vector);
+    vec3 light_dir = normalize(light_vector);
+
+    float diffuse_point = max(dot(normal, light_dir), 0.0);
+    
+    float attenuation = 1.0 / (distance * distance);
+    
+    float light_intensity = 2.0;
+    float point_light = diffuse_point * attenuation * light_intensity;
 
     float ambient = 0.3;
-    float lighting = max(diffuse + ambient, 0.0);
+    //float lighting = max(diffuse_dir + point_light + ambient, 0.0);
+    float lighting = max(point_light, 0.0);
 
     vec3 texture_color = texture(my_texture, frag_uv).rgb;
     vec3 shaded_color = texture_color * lighting;
--- res/vertex.glsl
+++ res/vertex.glsl
@@ -3,18 +3,19 @@
 #version 410
 
 uniform mat4 view_projection_matrix;
-uniform mat4 normal_matrix;
-
 layout(location = 0) in vec3 in_position;
 layout(location = 1) in vec2 in_uv;
 layout(location = 2) in vec3 in_normal;
 
 out vec2 frag_uv;
 out vec3 frag_normal;
+out vec3 frag_position;
 
 void main() {
-    gl_Position = view_projection_matrix * vec4(in_position, 1.0);
+    vec4 world_position = view_projection_matrix * vec4(in_position, 1.0);
+    gl_Position = world_position;
     
     frag_uv = in_uv;
-    frag_normal = normalize(mat3(normal_matrix) * in_normal);
+    frag_normal = normalize(in_normal);
+    frag_position = in_position; 
 }
